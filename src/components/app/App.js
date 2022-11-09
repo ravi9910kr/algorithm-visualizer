84c593ef579e6aa03a8246adcd6240e0e855ac6c
Ravi Kumar <ravikmar601@gmail.com>
2022-11-09 21:45:58 +0530

    some clean up

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import "./app.css";
-
+import Header from "../header/Header";
+import Canvas from "../canvas/Canvas";
+import Footer from "../footer/Footer";
 export default class App extends Component {
   render() {
-    return <div className="app">App</div>;
+    return (
+      <div className="app">
+        <Header />
+        <Canvas />
+        <Footer />
+      </div>
+    );
   }
 }
