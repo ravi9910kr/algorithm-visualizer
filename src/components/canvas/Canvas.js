5731963c3eb6138f269f8fd52d1dc6094d498e3d
Ravi Kumar <ravikmar601@gmail.com>
2022-11-10 17:59:57 +0530

    canvas can render an array now

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,6 +1,30 @@
-import React from "react";
-
+import React, { useRef, useEffect } from "react";
 const Canvas = () => {
-  return <canvas></canvas>;
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  const renderCurrentState = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      const wid = 1000 / arr.length,
+        x0 = i * wid;
+      drawUnit("#03045eff", x0, 0, wid, arr[i]);
+    }
+  };
+  const drawUnit = (color, x0, y0, wid, higt) => {
+    contextRef.current.clearRect(x0, y0, wid, 720);
+    contextRef.current.fillStyle = color;
+    contextRef.current.fillRect(x0, y0, wid, higt);
+  };
+  useEffect(() => {
+    canvasRef.current.width = 1000;
+    canvasRef.current.height = 720;
+    contextRef.current = canvasRef.current.getContext("2d");
+    console.log(contextRef.current);
+  }, []);
+  return (
+    <>
+      <canvas ref={canvasRef}></canvas>
+    </>
+  );
 };
 export default Canvas;
